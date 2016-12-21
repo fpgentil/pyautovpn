493c5dbd51fcaf3547185138e1dc2d0b47a60f61
Felipe Gentil <cdigentil@gmail.com>
2016-12-21 12:11:58 -0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyautovpn',
+      version='0.1',
+      description='Automatic One-time Password VPN connector for OSX',
+      url='http://github.com/fpgenitl/pyautovpn',
+      author='Felipe Gentil',
+      author_email='cdigentil@gmail.com',
+      license='MIT',
+      packages=['pyautovpn'],
+      zip_safe=False)
