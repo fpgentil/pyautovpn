b11e3d5032e19e96d00fc4ff4bc87e6605faaf7a
Felipe Gentil <cdigentil@gmail.com>
2016-12-23 13:04:29 -0200

    Escape strings and change os to commands

--- pyautovpn/__init__.py
+++ pyautovpn/__init__.py
@@ -1,24 +1,21 @@
-import os
-import commands
-import re
+import commands, re
 
 def setup(password, token, vpn_name):
     user = commands.getstatusoutput("whoami")[-1]
 
     keychain_pwd = commands.getstatusoutput("security -q find-generic-password -gl pyautovpn_pass | egrep '^password'")[-1]
     if (re.search('password\:', keychain_pwd) != None):
-        os.system("security -q delete-generic-password -a {0} -s pyautovpn_pass".format(user))
+        commands.getstatusoutput("security -q delete-generic-password -a {0} -s pyautovpn_pass".format(user))
+    commands.getstatusoutput("security -q add-generic-password -a {0} -s pyautovpn_pass -w {1}".format(user, re.escape(password)))
 
     keychain_token = commands.getstatusoutput("security -q find-generic-password -gl pyautovpn_token | egrep '^password'")[-1]
     if (re.search('password\:', keychain_token) != None):
-        os.system("security -q delete-generic-password -a {0} -s pyautovpn_token".format(user))
+        commands.getstatusoutput("security -q delete-generic-password -a {0} -s pyautovpn_token".format(user))
+    commands.getstatusoutput("security -q add-generic-password -a {0} -s pyautovpn_token -w {1}".format(user, re.escape(token)))
 
     keychain_name = commands.getstatusoutput("security -q find-generic-password -gl pyautovpn_name | egrep '^password'")[-1]
     if (re.search('password\:', keychain_name) != None):
-        os.system("security -q delete-generic-password -a {0} -s pyautovpn_name".format(user))
-
-    os.system("security -q add-generic-password -a {0} -s pyautovpn_pass -w {1}".format(user, password))
-    os.system("security -q add-generic-password -a {0} -s pyautovpn_token -w {1}".format(user, token))
-    os.system("security -q add-generic-password -a {0} -s pyautovpn_name -w {1}".format(user, vpn_name))
+        commands.getstatusoutput("security -q delete-generic-password -a {0} -s pyautovpn_name".format(user))
+    commands.getstatusoutput("security -q add-generic-password -a {0} -s pyautovpn_name -w {1}".format(user, re.escape(vpn_name)))
 
     print("Password,token and VPN name were added/updated successfully")
